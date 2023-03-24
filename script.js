eabe973b7a8ef74b17a0c88c35354167beb318d3
edgardjustino <eddiejustino@gmail.com>
2023-03-24 14:01:00 -0300

    changing js

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-//get elements
-let = [hour, min, sec] = [0, 0, 0]
+let [hour, min, sec] = [0, 0, 0];
+let displayTime = document.getElementById('time');
+let interval = null; 
 
-function startwatch () {
-    if (sec == 0){
-        sec++
+function start () {
+        sec++;
+        if (sec == 60){
+            sec = 0;
+            min++;
+
+            if ( min == 60) {
+                min = 0;
+                hour++;
+            }
+        }
+
+        let displayHour = hour < 10 ? '0' + hour : hour;
+        let displayMin = min < 10 ? '0' + min : min;
+        let displaySec = sec < 10 ? '0' + sec : sec;
+
+        time.innerHTML = displayHour + ':'+ displayMin +':'+displaySec;
+
+}
+
+function watchstart(){
+    setInterval(start,1000);
 }
