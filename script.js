0eeea8a8bc51b5eceb0c8afab0049179c5e7f334
edgardjustino <eddiejustino@gmail.com>
2023-03-26 23:06:34 -0300

    adjusts on js

--- script.js
+++ script.js
@@ -1,31 +1,74 @@
-let [hour, min, sec] = [0, 0, 0];
-let displayTime = document.getElementById('time');
+let hour = 0;
+let min = 0;
+let sec = 0;
+
+let displayHour;
+let displaySec;
+let displayMin;
 
 let status = 'stopped';
 
 let interval = null; 
 let lapNow = null;
 
-function startStop () {
-        sec++;
-        if (sec == 60){
-            sec = 0;
-            min++;
-
-            if ( min == 60) {
-                min = 0;
-                hour++;
-            }
+function start(){
+    sec++
+    if (sec == 60){
+        min++;
+        sec = 0;
+        if ( min == 60){
+            hour++;
+            min = 0;
         }
+    }
 
-        let displayHour = hour < 10 ? '0' + hour : hour;
-        let displayMin = min < 10 ? '0' + min : min;
-        let displaySec = sec < 10 ? '0' + sec : sec;
+    if(sec < 10){
+        displaySec = '0'+ sec;
+    } else {
+        displaySec = sec; 
+    }
 
-        time.innerHTML = displayHour + ':'+ displayMin +':'+displaySec;
+    if ( min < 10){
+        displayMin = '0' + min;
+    } else {
+        displayMin = min;
+    }
 
+    if (hour < 10){
+        displayHour = '0' + hour;
+    } else {
+        displayHour = hour;
+    }
+
+    document.getElementById('time').innerHTML = `${displayHour}:${displayMin}:${displaySec}`;
 }
 
-function watchstart(){
-    setInterval(start,1000);
+function startStop () {
+    if ( status === 'stopped'){
+      interval = window.setInterval(start, 1000);
+      status = 'started'
+      document.getElementById('start-btn').innerHTML = 'Stop';
+      document.getElementById("start-btn").style.background = "red";
+
+    } else {
+      window.clearInterval(interval);
+      status = 'stopped';
+      document.getElementById('start-btn').innerHTML = 'Start';
+      document.getElementById("start-btn").style.background = "rgba(9, 53, 108, 0.69)";
+    }
+}
+
+function reset (){
+    window.clearInterval(interval);
+    hour = 0;
+    min = 0;
+    sec = 0;
+    displayHour = 0;
+    displayMin = 0;
+    displaySec = 0;
+    status = 'stopped';
+    document.getElementById('time').innerHTML = "00:00:00"
+    document.getElementById('lap-time').innerHTML = '';
+    document.getElementById('start-btn').innerHTML = 'Start';
+    document.getElementById("start-btn").style.background = "rgba(9, 53, 108, 0.69)";
 }
